0c7e01f3d0a286bb3798734a3067c644fa4f5e58
Kaiser Ahmed Tushar <kaiser.tushar@yahoo.com>
2017-01-10 22:56:17 +0600

    Some Text Added about our BUBT Professionals

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.master')
 
 @section('content')
+<style>
+.name{
+  font-size: 30px!important;
+}
+</style>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.min.css">
     <!-- Header -->
     <header  style="background-color: #0f0f19;">
-        <div class="container" style="padding-top: 180px;">
+        <div class="container" >
             <div class="row">
                 <div class="col-lg-12" >
                     <!-- <div class="intro-text">
@@ -18,20 +23,36 @@
             </div>
         </div>
     </header>
-
-   <section class="container">
-       <div class="row">
-           <div class="col-md-offset-4 col-md-4 text-center">
-               <p align="center" class="text-center">
-                   If you want to participate BUBT IT Professionals Meetup and meet with the Professionals then you have to join first.
-               </p>
-
-               <a href="{{url('register')}}" class="btn btn-success btn-lg" id="sbmt">Join The Event</a>
-
+    <header style="background-color: white;color: black;">
+     <div class="container" style="padding-top: 50px!important;">
+         <div class="row">
+           <div class="intro-text">
+              <span class="name"> Who we are </span>
+              <hr class="star-primary" >
+               <span class="skills text-justify">
+                We are the professionals of BUBT who are dedicated to building our coummunity more stronger and our network more powerful.We believe that Working together will help us to make an awesome carrier.
+               </span>
+            </div>
+         </div>
+     </div>
+     <div class="container" style="padding-top: 0px!important;">
+         <div class="row">
+            <div class="intro-text">
+             <span class="name"> What we are planning for</span>
+             <hr class="star-primary" >
+             <span class="skills text-justify">
+                    BUBT Professionals is a platform where students of BUBT connect with each other and help to build an awesome community.Aim of BUBT Professionals is to make a strong professionals community.
+                    BUBT It Professionals Meetup, 2017 is going to happen on 10th February in our own campus for you,me and us.If you want to participate in BUBT IT Professionals Meetup to meet with the Professionals,you have to join first.
+             </span>
+                   
            </div>
-       </div>
-   </section>
 
+         </div>
+         <br>
+         <a href="{{url('register')}}" class="btn btn-success btn-lg" id="sbmt">Join The Event</a>
+     </div>
+
+  </header>
 
     <script type="text/javascript" src="/js/jquery.countdown.min.js"></script>
     <script type="text/javascript" src="/js/register.js"></script>
